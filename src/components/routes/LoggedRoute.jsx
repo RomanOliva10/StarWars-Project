3302c0da24c9beac05dc8bcd77077532e41a1822
matiashidalgo11 <matiashidalgo18@gmail.com>
2021-12-21 14:10:31 -0300

    [DELETE/FILES] delete signin and register

--- src/components/routes/LoggedRoute.jsx
+++ src/components/routes/LoggedRoute.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 
 // import context
 import { SessionContext } from '../../context/sessionContext';
-import SignIn from '../users/signIn/signIn';
 import UsersForms from '../users/formsUsers/userForms'
 
 export default function LoggedRoute({ children }) {
